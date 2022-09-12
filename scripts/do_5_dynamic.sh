6f2f3ea2a5fab4a6ea3478985fbaa868b0b9cf78
Jeremy Meiss <jeremy.meiss@gmail.com>
2022-09-12 09:00:38 -0500

    add DOTC22 Sydnet workshop

---
+++ scripts/do_5_dynamic.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# cp scripts/do/README.md .
+cp -f scripts/do/configs/config_5_setup.yml .circleci/config.yml
+cp -f scripts/do/configs/config_5_continue.yml .circleci/continue_config.yml
